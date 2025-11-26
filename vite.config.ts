471b4c2361da2949a7b5a0d6178cad9e6a26c6e6
Hugo GARCIA <hugo.gds2004@gmail.com>
2025-11-26 23:41:58 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true
-  },
-  server: {
-    proxy: {
-      '/v1': {
-        target: 'https://51.91.109.185:8001',
-        changeOrigin: true,
-        secure: false
-      },
-      // Proxy para el endpoint /token (si tu backend expone /token a la raíz)
-      '/token': {
-        target: 'https://51.91.109.185:8001',
-        changeOrigin: true,
-        secure: false
-      }
-    }
   }
 })
